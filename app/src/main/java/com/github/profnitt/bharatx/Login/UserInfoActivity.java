3123c437f479807a3fdaa1cc44ceb56e74df9196
Shyam <m.shyam.tnj@gmail.com>
2019-12-10 23:06:33 +0530

    implement optional BusinessInfoActivity for 'Student' and 'Other' types

--- app/src/main/java/com/github/profnitt/bharatx/Login/UserInfoActivity.java
+++ app/src/main/java/com/github/profnitt/bharatx/Login/UserInfoActivity.java
@@ -1,26 +1,39 @@
 package com.github.profnitt.bharatx.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.github.profnitt.bharatx.DashboardActivity;
 import com.github.profnitt.bharatx.R;
+import com.github.profnitt.bharatx.databinding.ActivityOtpBinding;
+import com.github.profnitt.bharatx.databinding.ActivityUserInfoBinding;
+
+import java.util.ArrayList;
 
 public class UserInfoActivity extends AppCompatActivity {
+ActivityUserInfoBinding binding;
+final int BusinessOptionIndex = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_info);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_user_info);
 
-        Button mButtonNext = findViewById(R.id.next_button);
-        mButtonNext.setOnClickListener(new View.OnClickListener() {
+        binding.nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), BusinessAccountInfoActivity.class);
+                Intent intent;
+
+                if (binding.employmentStatus.getSelectedItemPosition() != BusinessOptionIndex)
+                    intent = new Intent(getApplicationContext(), DashboardActivity.class);
+                else
+                    intent = new Intent(getApplicationContext(), BusinessAccountInfoActivity.class);
+
                 startActivity(intent);
             }
         });
