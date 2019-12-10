2f709f79cf3dba4d7a0db0d87fce5e1e39fcd2c4
Shyam <m.shyam.tnj@gmail.com>
2019-12-10 18:11:44 +0530

    Implement optional fields for BusinessAccountInfoActivity

--- app/src/main/java/com/github/profnitt/bharatx/Login/BusinessAccountInfoActivity.java
+++ app/src/main/java/com/github/profnitt/bharatx/Login/BusinessAccountInfoActivity.java
@@ -1,25 +1,52 @@
-package com.github.profnitt.bharatx;
+package com.github.profnitt.bharatx.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 
-public class BusinessAccountInfoActivity extends AppCompatActivity {
+import com.github.profnitt.bharatx.DashboardActivity;
+import com.github.profnitt.bharatx.R;
+import com.github.profnitt.bharatx.databinding.ActivityBusinessAccountInfoBinding;
 
+public class BusinessAccountInfoActivity extends AppCompatActivity {
+    final String DEBUG_TAG = "BusinessAccountInfo";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_business_account_info);
+        final ActivityBusinessAccountInfoBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_business_account_info);
 
-        Button mButtonSubmit = findViewById(R.id.submit_button);
-        mButtonSubmit.setOnClickListener(new View.OnClickListener() {
+        binding.submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        binding.businessAccountSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(DEBUG_TAG,  String.valueOf(position));
+                switch(position) {
+                    case 1:
+                        binding.businessAccountDetails.setVisibility(View.VISIBLE);
+                        break;
+
+                    case 0:
+                        binding.businessAccountDetails.setVisibility(View.GONE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                binding.businessAccountSpinner.setSelection(1);
             }
         });
     }
